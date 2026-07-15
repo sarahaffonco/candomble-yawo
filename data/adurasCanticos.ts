d5fe8bba8ccf5fedb5d2d83a9fb587ccf68ebe2b
Sarah Affonco <sarah.affonco@gmail.com>
2026-07-14 22:06:00 -0300

    feat/aduras

--- data/adurasCanticos.ts
+++ data/adurasCanticos.ts
@@ -1,19 +1,431 @@
 export const adurasCanticos = [
     {
+        slug: "adura-esu",
+        nome: "Adura Exú",
+        adura: `Iyìn o, iyìn o Èṣù n má gbọ̀ o 
+
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o 
+Iyìn o, iyìn o Èṣù n má gbọ̀ o 
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o 
+Èṣù láaróyè, Èṣù láaróyè
+     Iyìn o, iyìn o Èsù n má gbọ̀ o
+Èṣù (falar o nome do Èṣù)
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o 
+Èsù ọ̀ta òrìṣà
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o 
+Oṣètùrá ni l’orukọ bàbá mọ́ ọ́
+Alágogo ijà l’orukọ íyá npẹ́ o
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o 
+Èṣù Ọ̀dàrà, ọmọkùnrin Idọ́lófin
+O lé sónsó sóri ori esẹ̀ ẹlẹ́sẹ̀
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o
+ Kọ̀ jẹ́ kọ́ jẹ́ ki ẹni njẹ́ gbẹ e mi
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o
+A kìì lówó lái mu ti Èṣù kúrò
+A kìì láyọ̀ lái mu ti Èṣù Kúrò
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o
+Aṣòntún ṣe òsì làì ní ìtìjú
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o
+Èsù ápáta somo olómo lénu
+O fi okúta dipò iyó
+
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o
+Lóògemo òrun a nla kálu
+Pàápa-wàrá, a túká máṣe ṣà
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o
+Èṣù máṣe mi, omo elómiran ni o sé
+Èṣù máse, Èṣù máse, Èṣù máse
+     Iyìn o, iyìn o Èṣù n má gbọ̀ o `,
+        traducao: `Èṣù escute o meu louvor!
+
+     Èṣù escute o meu louvor!
+Èṣù escute o meu louvor!
+     Èṣù escute o meu louvor!
+Salve Èṣù! Salve Èṣù!
+     Èṣù escute o meu louvor!
+Èṣù (falar o nome do Èṣù)
+     Èṣù escute o meu louvor!
+Èṣù, o primeiro entre os orixás
+     Èṣù escute o meu louvor!
+Oṣètùrá é o nome pelo qual é chamado pelo pai
+Alágogo é o nome pelo qual é chamado pela mãe
+     Èṣù escute o meu louvor!
+Èṣù bondoso, filho da cidade de Idólófin
+De cabeça pontiaguda, está sempre na retaguarda
+     Èṣù escute o meu louvor!
+Não come e também não permite que comamos
+     Èṣù escute o meu louvor!
+Quem tem riqueza deve reservar a parte de Èṣù
+Quem tem felicidade deve reservar a parte de Èṣù
+
+     Èṣù escute o meu louvor!
+Ele fica dos dois lados sem constrangimento
+     Èṣù escute o meu louvor!
+Montanha de pedra que faz o filho falar o que não quer
+Aquele que usa pedra em lugar de sal
+     Èṣù escute o meu louvor!
+Filho do céu cuja grandeza está em todos os lugares
+Aquele que fragmenta o que não se pode nuca mais unir
+     Èṣù escute o meu louvor!
+Èsù não me faça mal, faça ao filho do outro
+Èsù não me faça mal, não me faça mão, não me faça mal
+     Èṣù escute o meu louvou! `
+    },
+    {
+        slug: "adura-ogum",
+        nome: "Adura Ogum",
+        adura: `Ògún dá lẹ́ kọ́
+Ẹni adé ran
+Ògún dá lẹ́ kọ́
+Ẹni adé ran
+Ògún to wa dó
+Ẹni adé ran
+Ògún to wa dó
+Ẹni adé ran `,
+        traducao: `Ògún constrói casa sozinho
+A mando do rei
+Ògún constrói casa sozinho
+A mando do rei
+Basta Ògún na construção da aldeia
+
+A mando do rei
+Basta Ògún na construção da aldeia
+A mando do rei `
+    },
+    {
+        slug: "adura-oxossi",
+        nome: "Adura Oxóssi",
+        adura: ` Pakó tòri sa(n) gbo dìdé (aja in pa igbo)
+Odẹ́ aróle o
+Aróle o oni sa(n) gbo olówo
+Odẹ́ aróle o nku lodẹ `,
+        traducao: `Fisga, mata e arrasta ferozmente sua presa
+(O cão morto na floresta)
+Ele é caçador herdeiro
+Hoje o herdeiro exibe sua riqueza
+Ele é o caçador herdeiro que tem o pode de atrair a caça para a morte. `
+    },
+    {
+        slug: "adura-logun",
+        nome: "Adura Logun-Edé",
+        adura: `Bábá ode ewé éjé,
+Ní igbó ní bàbá Òrìsà
+Ní igbó ni bábá Lògún ode
+Bàbá yìí dakójá mi ojú ààrò, 
+O ti kó là àse e tyìn ofà, 
+Bàbá igbó ode ènyin. `,
+        traducao: `Pai caçador há sangue nas folhas
+Pai caçador há sangue nas folhas
+O pai Orixá está nas florestas, está nas florestas o pai guerreiro caçador.
+Este pai Me observa olha a minha cooperação, ele foi o primeiro a tornar-se rico
+Com o poder do seu arco e flecha, pai das florestas e caçador és. `
+    },
+    {
+        slug: "adura-ossaim",
+        nome: "Adura Ossaim",
+        adura: `Meré-meré Ọ̀sányìn ewé o ẹ́ jin
+Meré-meré Ọ̀sányìn ewé o ẹ́ jin
+Meré-meré ewé o ẹ́ jin ngbẹ́ nọ́n
+Meré-meré ewé o ẹ́ jin ngbẹ́ nọ́n
+Ẹ jin meré-meré Ọ̀sányìn wa le `,
+        traducao: `Habilmente, Ọ̀sányìn vos dá as folhas
+Habilmente, Ọ̀sányìn vos dá as folhas
+Habilmente vos destes as folhas secas no caminho
+Habilmente vos destes as folhas secas no caminho
+Habilmente, Ọ̀sányìn vos destes a magia `
+    },
+    {
+        slug: "adura-omulu",
+        nome: "Adura Omulu",
+        adura: `Ọmọlú ìgbọ́ná ìgbóná zue
+Ọmọlú ígbọ́ná ìgbóná zue
+Ekọ ọmọ vodun maseto
+Ekọ ọmọ vodun na je
+Ìjòni le o Nàná
+Ìjòni le o Nàná ki mayò
+Nàná ki mayọ̀ ki n a lode
+Fẹ̀lẹ̀fẹ̀lẹ̀ mi igba nlọ, ajunsun wale
+Meré-meré ẹ no ile isin
+Meré-meré ẹ no ile isin
+E n sinbe meré-meré osú láyè
+E n sinbe meré-meré osú láyè
+Ọba alá tun zue obi osùn
+Ọba alá tun zue obi osùn
+Iya lóni
+Otù, àkọ́ba, bi ìyá, húkọ́, káká, bẹtọ
+Otun zue, obi, osùn
+Bara ale sọ ran ale sọ ran
+Bara otun zue obi osùn
+ `,
+        traducao: `Omolú, Senhor da Quentura Omolú
+Sempre febril produz saúde
+Educa o filho Vodun Maceto
+Vodun educa o filho castigando
+Nàná ele é capaz de provocar queimaduras
+Ele é capaz de provocar queimaduras, Nana, e se enche de alegria
+Nàná ele se enche de alegria do lado de fora
+És capaz de fazer definhar em vida, Ajunsun, até secar
+Habilmente ele enche a nossa casa de escravos
+Habilmente ele enche a nossa casa de escravos
+Primeiramente o erguemos habilmente osú que cobre a terra
+Primeiramente o erguemos habilmente osú que cobre a terra
+Rei que nasceu como o sol, Pai do Vermelho
+Rei que nasceu como o sol, Pai do Vermelho
+Neste dia
+Doença, infelicidade, sofrimento, tosse, dificuldades, aflição
+Suplico-lhe diariamente, Pai do Vermelho
+Rei do corpo suplico-lhe rastejando
+Rei do corpo suplico-lhe diretamente, Pai do vermelho `
+    },
+    {
+        slug: "adura-oxumare",
+        nome: "Adura Oxumarê",
+        adura: `Ọ̀ṣùmàrè e sé wa dé òjò
+Àwa gbè ló sìngbà opé wa
+E kun òjò wa
+Dájú e òjò odò
+Dájú e òjò odò s’àwa `,
+        traducao: `Ọ̀ṣùmàrè é quem nos traz a chuva
+Nós a recebemos e retribuímos agradecidos
+A chuva é bastante para nós...
+A chuva se transforma no rio
+Certamente vossa chuva é o rio, para nós. `
+    },
+    {
+        slug: "adura-iroko",
+        nome: "Adura Iroko",
+        adura: `ODARA ILE O ÌRÒKO
+ODARA ILE O ÒRISÁ ÌRÒKO
+ODARA ILE ODE KO ARO `,
+        traducao: ` `
+    },
+    {
+        slug: "adura-xango1",
+        nome: "Adura Xangô 1",
+        adura: ` Ọba ìró l’òkó
+Ọba ìró l’òkó
+Yá ma sé kun ayinra òjẹ
+(Aganju/Ogodo/Afonjá) òpó monjá le kòn
+Okàn olo l’Oyá
+Tobi fori òrìsà
+Ọba sorun alá alàgba òjẹ`,
+        traducao: `Rei do Trovão
+Rei do Trovão
+Encaminha o fogo sem errar o alvo, nosso vaidoso Òje
+(Aganju/Ogodo/Afonjá) alcançou o Palácio Real
+Ùnico que possuiu Oyá
+Grande Líder dos Orixás
+Rei que conversa no céu e que possui a honra dos Òje
+Rei que conversa no céu e que possui a honra dos Òje `
+    },
+    {
+        slug: "adura-xango2",
+        nome: "Adura Xangô 2",
+        adura: ` Oba Kao
+Oba Kao
+Kabiyesile
+Oba Ni Kole
+Oba sere
+Oba Njeje
+Se re Alado
+Bangbose O wo bitiko
+Osé Kao
+O Kabiyesile`,
+        traducao: `O Rei meus cumprimentos
+O Rei meus cumprimentos
+Sua Majestade
+O Rei construiu uma casa
+O Rei do xere
+O Rei prometeu, traz boa sorte, Dono do Pilão 
+Bamboxê abidikô meus cumprimentos
+Oxê Sua Majestade `
+    },
+    {
+        slug: "adura-oya",
+        nome: "Adura Oyá",
+        adura: ` E ma odò, e ma odò
+Lagbó lagbó méje
+Ọ dundun a sọrọ
+Balẹ̀ hey. `,
+        traducao: `Eu vou ao rio, eu vou ao rio
+Que nos arbustos se divide em sete
+Vós que fala através do Dundun
+Tocando o solo te saúdo. `
+    },
+    {
+        slug: "adura-oba",
+        nome: "Adura Exú",
+        adura: `Ọ̀bà mo pẹ o o
+Ọ̀bà mo pẹ o o
+Sare wa jẹ mi o
+Ọ̀bà ojowu aya Sàngó sare
+Wa gbọ àdúrà wa o
+Enì n wa owó, ki o fún ni owó
+Enì n wa ọmọ, ki o fún ni ọmọ
+Enì n wa àláfíà, ki o fún ni àláfíà
+Sare wa jẹ mi o. `,
+        traducao: ` Òbà eu vos chamo
+Òbà eu vos chamo
+Venha depressa nos atender
+Òbà, mulher ciumenta esposa de Sàngó, venha depressa
+Ouvir a nossa súplica
+A quem quer dinheiro, dá dinheiro
+A quem quer filhos, dá filhos
+A quem quer saúde, dá saúde
+Venha logo nos atender. `
+    },
+    {
         slug: "adura-oxum",
         nome: "Adura Oxum",
-        traducao: `
-Vós que gentilmente me dá muitos presente
-Calmamente sem aflição
-Mãe dos gêmeos que vem a ser mãe de Lógun, Rainha das águas pingando
-Os gêmeos adornam vários kòrì sem queimar
-Rainha me faz guisado em pequenas panelas deslumbrantemente corte com a espada
-    `,
-        adura: `Ẹ njì tenú ma mi o
+        adura: ` Ẹ njì tenú ma mi o
 Tenú màmà ya
 Ìyá Ìbẹjí di Lógun àyaba omi ro
 Ìbẹjì kórì ko jo
 Àyaba ma pákútá màlà ge sá
-Iya mi yèyé (Òsogbo / Ipondá / Opara / Kare).`
+Iya mi yèyé (Òsogbo / Ipondá / Opara / Kare).`,
+        traducao: `Vós que gentilmente me dá muitos presente
+Calmamente sem aflição
+Mãe dos gêmeos que vem a ser mãe de Lógun, Rainha das águas pingando
+Os gêmeos adornam vários k’òrì sem queimar
+Rainha me faz guisado em pequenas panelas deslumbrantemente corte com a espada `
+    },
+    {
+        slug: "adura-ewa",
+        nome: "Adura Yèwá",
+        adura: `Pẹ̀lẹ́ ‘nbo Yèwá a níre o
+Pẹ̀lẹ́ ‘nbo Yèwá a níre o
+Òrìṣà yin a ‘nbọ Yèwá
+Yèwá a níre o `,
+        traducao: `Delicadamente cultuamos Yèwá por estarmos felizes
+Delicadamente cultuamos Yèwá por estarmos felizes `
+    },
+    {
+        slug: "adura-nana",
+        nome: "Adura Nanã",
+        adura: `Ẹ kò odò, ẹ kò odò fó
+Ẹ kò odò, ẹ kò odò fó
+Ẹ kò odò, ẹ kò odò fó
+Ẹ kò odò, ẹ kò odò fó
+Kò odò, kò odò, kò odò ẹ
+Dura dura ní kò gbẹ̀ngbẹ̀
+Mawun awun a tì jô n
+Saluba Nàná, saluba Nàná, saluba.
+Saluba Nàná, saluba Nàná, saluba. `,
+        traducao: `Encontro-lhe no rio, encontro-lhe no leito do rio
+Encontro-lhe no rio, encontro-lhe no leito do rio
+Encontro-lhe no rio, encontro-lhe no leito do rio
+Encontro-lhe no rio, encontro-lhe no leito do rio
+Encontro no rio, encontro no rio, encontro-lhe no rio
+Esforçando-me para não afundar na travessia do grande rio
+Lentamente como uma tartaruga trancada suplicando perdão `
+    },
+    {
+        slug: "adura-iemanja",
+        nome: "Adura Iemanjá",
+        adura: `Yemonja gbé rere ku e sìngbà
+Gbà ní a gbè wí
+To bo sínú odò yin
+Òrìṣà ògìnyọ́n gbà ní odò yin `,
+        traducao: `Yemonja, traz boa sorte repentinamente retribuindo
+Receba-nos e proteja-nos em vosso rio
+Cultuamos-vos sufientimente em vosso rio
+Orixá comedor de inhames novos, receba-nos em vosso rio. `
+    },
+    {
+        slug: "adura-oxala",
+        nome: "Adura Oxalá",
+        adura: `Bàbá esá rè wa
+Ewa agba awo a sare wa
+A jẹ águtan
+A sare wa ewa agba awo
+Iba Òrìṣà yin agba ògìnyọ̀n `,
+        traducao: `Pai dos ancestrais, venha nos trazer boa sorte
+Belo ancião do mistério, venha depressa
+Comedor de ovelha
+Venha depressa belo ancião do mistério
+Saudações Orixá escute-me ancião comedor de inhame pilado. `
+    },
+    {
+        slug: "adura-ori1",
+        nome: "Adura Ori 1",
+        adura: `Orí ení kini sàka ení
+Orí ení kini sàka yan
+Orí olóore ori jẹ̀ o
+A saka yìn ki ya n’to lo ko
+A saka yìn ki ègbón mi gbẹ̀
+Ìta nù mo bo orí o. `,
+        traducao: `Cabeça que está purificada na esteira
+Cabeça que está purificada na esteira caminha soberbamente
+A cabeça do vencedor vencerá
+A cabeça limpa que louvamos mãe permita que façam uso dela
+A cabeça limpa que louvamos meu mais velho conduzirá
+Ar livre e limpo oferendo a cabeça `
+    },
+    {
+        slug: "adura-ori2",
+        nome: "Adura Ori 2",
+        adura: `Ájálá orí, orí l'ewa,l'ewa,l'ewa
+         Ájálá orí, orí l'ewa,l'ewa,l'ewa,
+        Opé èyin Eledúmare, wá ori e kú ó
+         Opé èyin Eledúmare, wá ori e kú ó
+         E kú ó órun, e kú ó òsùpá, e kú òjo
+        òjò bò ilè
+         E kú ó órun, e kú ó òsùpá, e kú òjo
+        òjò bò ilè
+        Ire orì ó ójí, ójí ire orí
+        Ire orí í ójí, ójí ire orí`,
+        traducao: `Orixa que molda as cabeças, a cabeça está linda, linda, linda
+        Orixa que molda as cabeças, a cabeça está linda, linda, linda
+        Nós agradecemos a Eledúmare, venha cabeça e seja abençoada
+        Saudamos o sol, saudamos a lua, saudamos a chuva que cai na terra
+        Feliz a cabeça acorede, acorde feliz a cabeça!
+        Feliz a cabeça acorede, acorde feliz a cabeça!`
+    },
+    {
+        slug: "adura-yami1",
+        nome: "Adura Iyami 1",
+        adura: `Ìyá kéré gbo ìyámi o
+Ìyá kéré gbohùn mi
+Ìyá kéré gbo ìyámi o
+Ìyá kéré gbohùn mi
+Gbogbo Ẹlẹ́yẹ mo Ìgbàtí
+Ìgbàmú ilẹ
+Ìyá kéré gbohùn mi
+Gbogbo Ẹlẹ́yẹ mo Ìgbàtí
+Ìgbàmú ilẹ
+Ìyá kéré gbohùn mi `,
+        traducao: `Pequeninas mães, ó idosas mães
+Pequeninas mães, ouçam minha voz
+Pequeninas mães, ó idosas mães
+Pequeninas mães, ouçam minha voz
+Todas as senhoras dos pássaros, ouçam-me 
+Quando eu as chamo na terra
+Pequeninas mães, ouçam minha voz
+Todas as senhoras dos pássaros da noite
+Todas as vezes que comprimo a terra
+Pequeninas mães, ouçam minha voz `
+    },
+       {
+        slug: "adura-yami2",
+        nome: "Adura Iyami 2",
+        adura: `Mo júbà ènyin Ìyámi Òṣòròngá
+O tònón ẹ̀jẹ̀ enun a
+O tòokòn ẹ̀jẹ̀ èdọ̀
+Mo júbà ènyin Ìyámi Òṣòròngá
+O tònón ẹ̀jẹ̀ enun a
+O tòokòn ẹ̀jẹ̀ èdọ̀
+Ẹ̀jẹ̀ ó yè ní kálè o
+Ó yíyè, yíyè, yèyé kòkò
+Ó yíyè, yíyè, yèyé kòkò`,
+        traducao: `Meus respeitos a vós, minha mãe Oxorongá
+Vós que seguíeis os rastros do sangue interior
+Vós que seguíeis os rastros do coração e do sangue do fígado.
+Meus respeitos a vós, minha mãe Oxorongá
+Vós que seguíeis os rastros do sangue interior
+Vós que seguíeis os rastros do coração e do sangue do fígado.
+O sangue vivo que é recolhido pela terra cobre-se de fungos,
+E ele sobrevive, sobrevive, ó mãe muito velha.
+O sangue vivo que é recolhido pela terra cobre-se de fungos,
+E ele sobrevive, sobrevive, ó mãe muito velha`
     },
 ];
